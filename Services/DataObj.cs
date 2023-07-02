96363248c8b86c2f4e61200226d113e7443e56c1
Mungert69 <contact@mahadeva.co.uk>
2023-07-02 23:26:12 +0100

    ammeded property name

--- Services/DataObj.cs
+++ Services/DataObj.cs
@@ -8,7 +8,7 @@ namespace NetworkMonitor.Api.Services
 
        public class DataObj
     {
-        private string _setupNetworkMonitor = "To setup a service monitor visit : https://freenetworkmonitor.click";
+        private string _setupFreeNetworkMonitor = "To setup a service monitor visit : https://freenetworkmonitor.click";
 
         private string _testedAddress = "";
         private ushort? _testedPort = 0;
